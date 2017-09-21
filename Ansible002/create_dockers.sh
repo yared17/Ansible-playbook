4aa4cbf533d940ce0495991c1992febc37a316f4
Yared Henriquez Blanquicett <30659662+yared17@users.noreply.github.com>
2017-09-21 12:14:25 -0500

    Update create_dockers.sh

--- Ansible002/create_dockers.sh
+++ Ansible002/create_dockers.sh
@@ -9,13 +9,13 @@ echo
 echo "----------------------"
 echo "Lanzando servidor MySQL"
 #docker run -d -P --name server02 -p 2222:22 -p 3306:3306 $1
-docker run --name server02 -p 3306:3306 -v $DATA/mysql:/var/lib/mysql -e MYSQL_ROOT_PASW$
+docker run --name server02 -p 3306:3306 -v $DATA/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag
 echo "Servidor desplegado."
 echo
 echo "----------------------"
 echo "Lanzando servidor noSQL Redis"
 #docker run -d -P --name server03 -p 2223:22 -p6379:6379  $1
-docker run --name server03 -p 6379:6379 -v $DATA/redis:/data -d redis redis-server --ope$
+docker run --name server03 -p 6379:6379 -v $DATA/redis:/data -d redis:alpine redis-server --appendonly yes
 echo "Servidor desplegado."
 echo
 echo "----------------------"
